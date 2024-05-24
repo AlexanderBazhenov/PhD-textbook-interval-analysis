12e263f59a562d01cd32c31b80e8fd8f19a33970
AlexanderBazhenov <59746051+AlexanderBazhenov@users.noreply.github.com>
2024-05-24 11:40:38 +0300

    Add files via upload

---
+++ ExamplePlace.m
@@ -0,0 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%     PLACE    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+##dirroot = 'D:\Data\ST\2023\T\'
+##dirki = 'D:\Data\ST\2023\T\kinterval-0.0.1'
+##dirOld =  'd:\Data\ST\2022\T\'
+% HomePC
+dirroot = 'D:\ST\2024\T\'
+dirki = 'D:\ST\2023\T\kinterval-0.0.1'
+dirOld2022 =  'd:\ST\2022\T\'
+dirOld2023 =  'd:\ST\2023\T\'
+% FTI
+##dirroot ='E:\Users\Public\Documents\ST\2024\T\'
+##dirki = 'E:\Users\Public\Documents\ST\2023\T\kinterval-0.0.1'
+##dirOld2022 =  'E:\Users\Public\Documents\ST\2022\T\'
+##dirOld2023 =  'E:\Users\Public\Documents\ST\2023\T\'
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+addpath(dirroot)
+addpath(dirOld2022)
+addpath(dirOld2023)
+cd(dirroot)
